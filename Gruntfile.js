3644f3020cd69d89ebc73d055e8906d49afcfdfb
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2018-03-19 15:41:08 +1100

    Support --root for `npm start`
    This overlays the --root directories over the reveal.js source directory,
    allowing simple sharing of reveal.js amongst multiple presentations.
    
    This appears to be a bug-fix, as this was already attempting to serve
    the root directories, but when doing so, the reveal.js directories were
    not visible.
    
    Either that, or the --root parameter was only expected to be used when
    reveal.js was within the hiearchy and accessed using a relative path
    from the index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,7 +94,7 @@ module.exports = function(grunt) {
 			server: {
 				options: {
 					port: port,
-					base: root,
+					base: root.concat( [ '.' ] ),
 					livereload: true,
 					open: true,
 					useAvailablePort: true
